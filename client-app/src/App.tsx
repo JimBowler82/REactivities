97ffc6bb74da3984badb849ac6c3bdb0e791392d
James Bowler <jamesbowler@Jamess-MacBook-Pro.local>
2023-04-10 16:05:20 +0100

    end of section 3

---
+++ client-app/src/App.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+import { Header, List } from "semantic-ui-react";
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  const [activities, setActivities] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/activities").then((res) => {
+      setActivities(res.data);
+    });
+  }, []);
+
+  return (
+    <div>
+      <Header as="h2" icon="users" content="Reactivities" />
+      <List>
+        {activities.map((activity: any) => (
+          <List.Item key={activity.id}>{activity.title}</List.Item>
+        ))}
+      </List>
+    </div>
+  );
+}
+
+export default App;
---
+++ client-app/src/index.tsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "semantic-ui-css/semantic.min.css";
+import "./index.css";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(<App />);
